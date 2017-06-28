5904941f7eab3699e296669db5a7e895105e95d3
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-28 14:48:52 +0530

    Update BEAUTY.C

--- BEAUTY.C
+++ BEAUTY.C
@@ -1,1 +1,21 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the values");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the %d is positive");
+}
+else if(a<0)
+{
+printf("the %d is negative");
+}
+else
+printf("it is zero");
+getch();
+}
